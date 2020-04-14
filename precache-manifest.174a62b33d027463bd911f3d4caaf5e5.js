3f3874d9f160c7e0e760ae1b8cbdcf8660a48092
Roberto Rosa Junior <beto.junior@gmail.com>
2020-04-14 16:39:52 -0300

    Updates

---
+++ precache-manifest.174a62b33d027463bd911f3d4caaf5e5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d09cecd50941b2e1659741c07bbe59a",
+    "url": "/covid19monitor/index.html"
+  },
+  {
+    "revision": "9aaf826c294d24d6e676",
+    "url": "/covid19monitor/static/css/2.3aa04c6f.chunk.css"
+  },
+  {
+    "revision": "7013d35a2986b8fa30dd",
+    "url": "/covid19monitor/static/css/main.cb773ea1.chunk.css"
+  },
+  {
+    "revision": "9aaf826c294d24d6e676",
+    "url": "/covid19monitor/static/js/2.0667f764.chunk.js"
+  },
+  {
+    "revision": "aa754ba9f73776ab5291549b6092b0f0",
+    "url": "/covid19monitor/static/js/2.0667f764.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7013d35a2986b8fa30dd",
+    "url": "/covid19monitor/static/js/main.6a39c3e5.chunk.js"
+  },
+  {
+    "revision": "02ea4c6c08b87cf4f020",
+    "url": "/covid19monitor/static/js/runtime-main.7239c4ee.js"
+  }
+]);
